2d64d8104b437c1e517947e12218fa6ee9b12f89
ibogatac <ibogatac@veleri.hr>
2020-05-24 20:33:53 +0200

    Commit 3
    Uređivanje početne stranice, priprema za izradu login-a i registera,

--- projekt/src/app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Knjiga
-
-class KnjigaForm(forms.ModelForm):
-
-	class Meta:
-		model = Knjiga
-		fields = ['naziv', 'autor', 'izdavacka_kuca', 'godina_izdanja']
