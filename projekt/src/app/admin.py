5eb405703d211efd4f8870e91759717138a4f0fa
ibogatac <ibogatac@veleri.hr>
2020-05-24 15:07:08 +0200

    commit 2
    Mjenjanje modela

--- projekt/src/app/admin.py
+++ projekt/src/app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Knjiga
+from .models import Knjiga, KnjigaIzdanje
 from .forms import KnjigaForm
 
 class KnjigaAdmin(admin.ModelAdmin):
-	list_display = ['naziv', 'autor', 'izdavacka_kuca', 'godina_izdanja']
+	list_display = ['naziv', 'autor', 'izdavacka_kuca', 'godina_izdanja',]
 	form = KnjigaForm
 	list_filter = ['autor']
 	search_fields = ['naziv', 'autor', 'izdavacka_kuca']
@@ -13,3 +13,4 @@ class KnjigaAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Knjiga, KnjigaAdmin)
+admin.site.register(KnjigaIzdanje)
--- projekt/src/app/urls.py
+++ projekt/src/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Knjiznica-home'),
-    path('about/', views.about, name='Knjiznica-about'),
+	path('', views.home, name='Knjiznica-home'),
+    path('pknjige/', views.pknjige, name='Knjiznica-pknjige'),
+    path('nknjige/', views.nknjige, name='Knjiznica-neposudjene knjige'),
 ]
